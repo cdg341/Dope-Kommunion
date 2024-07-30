eed79a7eff3c50f4ce37c71ee519f6bde9b28ce3
Cameron <cdg341@gmail.com>
2024-07-29 19:19:33 -0500

    added more sections

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', ()=> {
+  console.log(window.scrollY);
+  if(window.scrollY > 30) {
+    header.classList.add('active');
+    console.log('test');
+  } else if(window.scrollY == 0) {
+    header.classList.remove('active')
+  }
+})
