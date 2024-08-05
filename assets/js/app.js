4ef522c866774209fd74190f74c9ff4c02dce869
Cameron <cdg341@gmail.com>
2024-08-05 14:18:46 -0500

    add mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,38 @@ window.addEventListener('scroll', ()=> {
     header.classList.remove('active')
   }
 })
+
+const mobileButton = document.querySelector('.mobile-button');
+const mobileMenu = document.querySelector('.mobile-menu');
+const body = document.querySelector('body');
+const bar = document.querySelectorAll('.bar');
+const menuLink = document.querySelectorAll('.menu-item a');
+
+mobileButton.addEventListener('click', () => {
+  if(!mobileMenu.classList.contains('active')){
+    mobileMenu.classList.add('active');
+    body.classList.add('overlay-active');
+    bar.forEach((bars) => {
+      bars.classList.remove('back');
+      bars.classList.add('change');
+    });
+  } else {
+    mobileMenu.classList.remove('active');
+    body.classList.remove('overlay-active');
+    bar.forEach((bars) => {
+      bars.classList.remove('change');
+      bars.classList.add('back');
+    });
+  }
+})
+
+menuLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.remove('active');
+    body.classList.remove('overlay-active');
+    bar.forEach((bars) => {
+      bars.classList.remove('change');
+      bars.classList.add('back');
+    });
+  })
+});
